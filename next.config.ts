0fc041af12cba7cbcee51807e8178aed8405c517
Rion <info@ri0n.dev>
2026-07-27 21:03:13 +0900

    feat: add remote image pattern for Unsplash to next.config.ts for improved image handling

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,14 @@ const nextConfig: NextConfig = {
   },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
